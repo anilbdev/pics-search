7736ec579bf8b58bf505b3e551771bfb9764a218
Anil Bhasi <anilbhasib@gmail.com>
2021-10-17 18:36:13 +0530

    Search Bar boilerplate created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import SearchBar from "./SearchBar";
+
 
 
 const App = () => {
     return (
         <div>
-            App
+            <SearchBar/>
         </div>
     )
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+    render(){
+        return(
+            <div>Seach Bar</div>
+        )
+    }
+}
+
+export default SearchBar
