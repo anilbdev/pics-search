f71d313ea7e1bf11c631c764ff991c7cfc5d3117
Anil Bhasi <anilbhasib@gmail.com>
2021-10-18 06:01:38 +0530

    search bar made controlled element

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 class SearchBar extends React.Component{
 
-    
+    state={term:''}
+
     render(){
         return(
             <div className="ui segment">
                 <form className="ui form">
                     <label>Image Search</label>
-                    <input type="text" onChange={(event)=>{console.log(event.target.value);}}/>
+                    <input type="text" value={this.state.term} onChange={(event)=>{this.setState({term:event.target.value})}}/>
                 </form>
             </div>
         )
