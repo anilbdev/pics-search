93c8488bb24a0f4c57e0281b944276ef06e5166f
Anil Bhasi <anilbhasib@gmail.com>
2021-10-18 06:27:31 +0530

    default reloading caused by form is prevented

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,12 +4,16 @@ class SearchBar extends React.Component{
 
     state={term:''}
 
+    onFormSubmit(event){
+        event.preventDefault()
+    }
+
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <label>Image Search</label>
-                    <input type="text" value={this.state.term} onChange={(event)=>{this.setState({term:event.target.value})}}/>
+                    <input type="text" value={this.state.term} onChange={(event)=>{this.setState({term:event.target.value.toUpperCase()})}}/>
                 </form>
             </div>
         )
