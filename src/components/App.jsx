b0e73ee2d129643d36a96742a57f6d765defcf6e
Anil Bhasi <anilbhasib@gmail.com>
2021-10-18 09:56:12 +0530

    context error corrected-no of images printed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,24 @@ import SearchBar from "./SearchBar";
 
 
 class App extends React.Component{
-    async onSearchSubmit(term){
-        
+    state ={images:[]}
+
+    onSearchSubmit = async (term) =>{        
         const response = await axios.
             get('https://api.unsplash.com/search/photos',{
                 params:{query:term},
                 headers:{
                     Authorization: 'Client-ID omoyuzAQm-x8qEWx0lMNdSuIIEahcLeO5JxKyOs0oP4'
                 }
-            })
-        console.log(response.data.results);
+            })        
+        this.setState({images:response.data.results})
     }
 
     render(){
     return (
         <div className="ui container" style={{marginTop:'10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit}/>
+            Found : {this.state.images.length} images
         </div>
     )}
 }
